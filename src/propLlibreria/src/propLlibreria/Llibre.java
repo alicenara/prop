464ca00fbec8a7de706194e325bbd6032f4ed5d1
Lucia <alicenara165@gmail.com>
2014-04-22 12:22:16 +0200

    test2

--- src/propLlibreria/src/propLlibreria/Llibre.java
+++ src/propLlibreria/src/propLlibreria/Llibre.java
@@ -4,7 +4,7 @@ public class Llibre {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.printf("hello pony :D");
+		System.out.printf("hello :D");
 	}
 
 }
