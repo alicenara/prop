84ee26ea71b6503a7e48acd1feb40a0cf394453d
RubenRam <ruben.bcn@hotmail.com>
2014-05-02 12:53:45 +0200

    asdf

--- src/propLlibreria/Area.java
+++ src/propLlibreria/Area.java
@@ -2,25 +2,13 @@ package propLlibreria;
 import java.util.*;
 
 public class Area {
+	private final int ID;
 	private String nomArea;
-	private ArrayList<Seccio> seccions;
-	
-	//pre:
-	//post: si existeix al paràmetre implicit una seccio amb nom "seccio" retorna 0 <= i < n,
-	//sent n el numero de seccions. En cas contrari retorna -1
-	private int getPosSeccio(String seccio) {
-		int n = seccions.size();
-		for (int i = 0; i < n; i++) {
-			if (seccions.get(i).getNomSeccio() == seccio) return i;
-		}
-		return -1;
-	}
 	
 	//pre:
 	//post: es crea una àrea amb nom "nomArea"
 	public Area (String nomArea){
 		this.nomArea = nomArea;
-		this.seccions = new ArrayList<Seccio>();
 	}
 	//pre:
 	//post: retorna nom de l'area
@@ -29,35 +17,6 @@ public class Area {
 	}
 	
 	public void afegirSeccio(String nom_seccio) {
-		Seccio nova_seccio = new Seccio(nom_seccio);
-		seccions.add(nova_seccio);
-	}
-	
-	//pre:
-	//post: elimina seccio de l'àrea amb nom "seccio"
-	public void esborrarSeccio(String seccio) {
-		int pos = getPosSeccio(seccio);
-		if (pos != -1) seccions.remove(pos);
-	}
-	
-	public void esborrarArea() {
-		Runtime garbage = Runtime.getRuntime();
-	    garbage.gc();
-	}
-	
-	//pre:
-	//post: retorna el numero de seccions dins d'una area
-	public int getNumSeccions() {
-		return this.seccions.size();
-	}
-	
-	//pre:
-	//post:retorna la seccio contiguda a àrea amb nom de seccio "seccio". En cas contrari retorna null.
-	public Seccio getSeccio(String seccio) {
-		int n = seccions.size();
-		for (int i = 0; i <n ; i++) {
-			if (seccions.get(i).getNomSeccio() == seccio) return seccions.get(i);
-		}
-		return null;
+		BD.
 	}
 }
--- src/propLlibreria/Seccio.java
+++ src/propLlibreria/Seccio.java
@@ -5,8 +5,6 @@ public class Seccio {
 	private String nomSeccio;
 	private final int ID;
 	
-
-	
 	//pre:
 	//post: crea una seccio amb nom "nomSeccio"
 	public Seccio (String nomSeccio) {
@@ -15,20 +13,20 @@ public class Seccio {
 	}
 	
 	//pre:
-	//post: paràmetre implícit passa a tenir com a nom de secció "nomSeccio"
+	//post: parametre implicit passa a tenir com a nom de secció "nomSeccio"
 	public void setNomSeccio(String nomSeccio) {
 		this.nomSeccio = nomSeccio;
 	}
 	//pre:
-	//post: retorna el nom de secció del paràmetre implícit
+	//post: retorna el nom de secció del parametre implicit
 	public String getNomSeccio() {
 		return this.nomSeccio;
 	}
 	
-	//pre:	el paràmetre implícit no conté una temàtica amb nom "nomTematica"
-	//post: afegeix una temàtica al paràmetre implícit
+	//pre:	el parametre implicit no conte una tematica amb nom "nomTematica"
+	//post: afegeix una tematica al parametre implicit
 	public void afegirTematica(String nomTematica) {
-		BD.getTematicaPerSeccio();
+		BD.getTematicaPerSeccio(nomTematica);
 	}
 	
 }
