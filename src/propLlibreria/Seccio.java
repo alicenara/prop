c7c19abe3a9b3a1ebebafbac584063829cd99a1f
RubenRam <ruben.bcn@hotmail.com>
2014-05-02 12:49:45 +0200

    sdf

--- src/propLlibreria/Seccio.java
+++ src/propLlibreria/Seccio.java
@@ -3,30 +3,15 @@ import java.util.ArrayList;
 
 public class Seccio {
 	private String nomSeccio;
-	private ArrayList<Tematica> tematiques;
+	private final int ID;
 	
-	//pre:
-	//post: si exiteix una tematica amb nom "nomTematica" retorna 0 <= i < n,
-	//sent n el numero de tematiques. En cas contrari retorna -1
-	private int getPosTematica(String nomTematica) {
-		int n = tematiques.size();
-		for (int i = 0; i < n; i++) {
-			if (tematiques.get(i).getNomTematica() == nomTematica) return i;
-		}
-		return -1;
-	}
+
 	
 	//pre:
 	//post: crea una seccio amb nom "nomSeccio"
 	public Seccio (String nomSeccio) {
 		this.nomSeccio = nomSeccio;
-	}
-	
-	//pre:
-	//post: el paràmetre implícit es eliminat
-	public void esborrarSeccio() {
-		Runtime garbage = Runtime.getRuntime();
-	    garbage.gc();
+		ID = BD.novaSecio(this);
 	}
 	
 	//pre:
@@ -43,13 +28,7 @@ public class Seccio {
 	//pre:	el paràmetre implícit no conté una temàtica amb nom "nomTematica"
 	//post: afegeix una temàtica al paràmetre implícit
 	public void afegirTematica(String nomTematica) {
-		this.tematiques.add(new Tematica(nomTematica));
+		BD.getTematicaPerSeccio();
 	}
 	
-	//pre:
-	//post: el paràmetre implícit veu reduit el numero de tematiques en un
-	public void esborrarTematica(String nomTematica) {
-		int pos = getPosTematica(nomTematica);
-		if (pos != -1) tematiques.remove(pos);
-	}
 }
