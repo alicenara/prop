437af251d52d202916730a4c07203d5c3dc3711c
sercco <sergibdn94@gmail.com>
2014-05-03 12:04:48 +0200

    Funciones casos de uso Area Seccio Tematica

--- src/propLlibreria/Area.java
+++ src/propLlibreria/Area.java
@@ -6,14 +6,21 @@ public class Area {
 	private String nomArea;
 	
 	//pre:
-	//post: es crea una àrea amb nom "nomArea"
+	//post: es crea una Area amb nom "nomArea"
 	public Area (String nomArea){
 		this.nomArea = nomArea;
+		ID = BD.novaArea(this);
 	}
 	//pre:
 	//post: retorna nom de l'area
 	public String getNomArea() {
 		return this.nomArea;
+	}
+	
+	//pre:
+	//post: el nom de l'area ara es nomArea
+	public String setNomArea(nomArea) {
+		this.nomArea = nomArea;
 	}
 	
 	public void afegirSeccio(Seccio novaSeccio) {
--- src/propLlibreria/Biblioteca.java
+++ src/propLlibreria/Biblioteca.java
@@ -7,21 +7,114 @@ public class Biblioteca {
     public Biblioteca() {
     	BD.init();
     }
-
+    
     //Methods
-	public void ordenarLlibres() {
-	//Utilitzar classe Solució
+    //pre:
+    //post: Llibres classificats segons l'afinitat entre ells i el nombre d'estanteries disponibles a la Biblioteca
+	public void reordenacioBiblioteca() {
+        //Utilitzar classe Solucio
     }
-
-	public matriu consultarOrdreActual() {
-    //Descobrir com declarar matriu
+    //pre:
+    //post: retorna l'ordre de classificacio dels llibres de la Biblioteca
+--->public matriu consultarOrdreActual() {
+        //Descobrir com declarar matriu
     }
-
-	public void afegirEstanteria(Estanteria novaEstanteria) {
-        estanteries.add(novaEstanteria);
+	
+	//pre: No existeix una Area, tal que el seu nom sigui nomArea
+    //post: S'ha creat una nova Area amb nom = nomArea
+	public void afegirArea(String nomArea) {
+		
 	}
-
-	public void esborrarEstanteria(Estanteria badEstanteria) {
-		estanteries.remove(badEstanteria);
+	
+	//pre: Existeix una Area tal que el seu Identificador = ID
+    //post: L'Area amb identificador ID te nom nomA
+	public void modificarNomArea(int ID, String nomA) {
+		
 	}
+	
+	//pre: Existeix una Area tal que el seu Identificador = IDA i una Seccio tal que el seu identificador es IDS i no esta continguda en l'Area
+    //post: L'Area amb identificador IDA conte la Seccio amb identificador IDS 
+	public void afegirSeccioArea(int IDA, int IDS) {
+		
+	}
+	
+	//pre: Existeix una Area tal que el seu Identificador = IDA i una Seccio tal que el seu identificador es IDS i esta continguda en l'Area
+    //post: L'Area amb identificador IDA no conte la Seccio amb identificador IDS 
+	public void esborrarSeccioArea(int IDA, int IDS) {
+		
+	}
+	
+	//pre: Existeix una Area tal que el seu Identificador = ID
+    //post: L'Area amb identificador = ID ha estat eliminada
+	public void eliminarArea(int ID) {
+		
+	}
+	
+	//pre: No existeix una Seccio, tal que el seu nom sigui nomSeccio
+    //post: S'ha creat una nova Seccio amb nom = nomSeccio
+	public void afegirSeccio(String nomSeccio) {
+		
+	}
+	
+	//pre: Existeix una Seccio tal que el seu Identificador = ID
+    //post: La Seccio amb identificador ID te nom nomS
+	public void modificarNomSeccio(int ID, String nomS) {
+		
+	}
+	
+	//pre: Existeix una Seccio tal que el seu Identificador = IDS i una Tematica tal que el seu identificador es IDT i no esta continguda en la Seccio
+    //post: La Seccio amb identificador IDS conte la Tematica amb identificador IDT 
+	public void afegirTematicaSeccio(int IDS, int IDT) {
+		
+	}
+	
+	//pre: Existeix una Seccio tal que el seu Identificador = IDS i una Tematica tal que el seu identificador es IDT i esta continguda en la Seccio
+    //post: La Seccio amb identificador IDS no conte la Tematica amb identificador IDT
+	public void esborrarTematicaSeccio(int IDS, int IDT) {
+		
+	}
+	
+	//pre: Existeix una Seccio tal que el seu Identificador = ID
+    //post: La Seccio amb identificador = ID ha estat eliminada
+	public void eliminarSeccio(int ID) {
+		
+	}
+	
+	//pre: No existeix una Tematica, tal que el seu nom sigui nomTematica
+    //post: S'ha creat una nova Tematica amb nom = nomTematica
+	public void afegirTematica(String nomTematica) {
+		
+	}
+	
+	//pre: Existeix una Tematica tal que el seu Identificador = ID
+    //post: La Tematica amb identificador ID te nom nomT
+	public void modificarNomTematica(int ID, String nomT) {
+		
+	}
+	
+	//pre: Existeix una Tematica tal que el seu Identificador = IDT i un Llibre tal que el seu identificador es IDL i no esta contingut en la Tematica
+    //post: La Tematica amb identificador IDT conte el Llibre amb identificador IDL 
+	public void afegirLlibreTematica(int IDT, int IDL) {
+		
+	}
+	
+	//pre: Existeix una Tematica tal que el seu Identificador = IDT i un Llibre tal que el seu identificador es IDL i esta contingut en la Tematica
+    //post: La Tematica amb identificador IDT no conte el Llibre amb identificador IDL 
+	public void esborrarLlibreTematica(int IDT, int IDL) {
+		
+	}
+	
+	//pre: Existeix una Tematica tal que el seu Identificador = ID
+    //post: La Tematica amb identificador = ID ha estat eliminada
+	public void eliminarTematica(int ID) {
+		
+	}
+    
+    public void afegirEstanteria() {
+        
+    }
+    
+    public void esborrarEstanteria() {
+        
+    }
 }
