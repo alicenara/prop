88312f310634c37626dad8b60d2573c1e4cd54b0
Lucia <alicenara165@gmail.com>
2014-05-20 11:50:11 +0200

    Començada interficie

---
+++ src/propLlibreria/Interficie/CtrlInterficie.java
@@ -0,0 +1,19 @@
+package propLlibreria.Interficie;
+import propLlibreria.Domini.CtrlBiblioteca;
+import java.util.*;
+/**
+ *
+ * @author Alice
+ */
+public class CtrlInterficie {
+    
+    private CtrlBiblioteca ctrlDomini;
+    /*private VistaPrincipal vistaPrincipal = null;
+    private VistaSecundaria vistaSecundaria = null;
+   */ 
+    
+    public void inicializarInterficies(){
+        
+    }
+    
+}
---
+++ src/propLlibreria/Interficie/Main.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package propLlibreria.Interficie;
+
+/**
+ *
+ * @author Alice
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {        
+        javax.swing.SwingUtilities.invokeLater (
+            new Runnable() {
+                public void run() {
+                    CtrlInterficie ctrlInterficie = new CtrlInterficie();
+                    ctrlInterficie.inicializarInterficies();
+                }
+            }
+        );
+    }
+    
+}
