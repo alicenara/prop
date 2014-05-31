282d249e0d81b622c631ef2b7e05acebcab71465
Lucia <alicenara165@gmail.com>
2014-05-31 12:04:30 +0200

    Vista Principal chorrada

--- src/propLlibreria/Interficie/VistaPrincipal.java
+++ src/propLlibreria/Interficie/VistaPrincipal.java
@@ -58,7 +58,7 @@ public final class VistaPrincipal extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setTitle("PROP");
         setMaximumSize(new java.awt.Dimension(500, 500));
         setMinimumSize(new java.awt.Dimension(500, 500));
