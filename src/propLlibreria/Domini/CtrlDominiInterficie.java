ce5d36de4019afbf477639afa5cd1d1e4f1c82d3
sercco <sergibdn94@gmail.com>
2014-06-04 19:35:14 +0200

    wev

--- src/propLlibreria/Domini/CtrlDominiInterficie.java
+++ src/propLlibreria/Domini/CtrlDominiInterficie.java
@@ -692,8 +692,7 @@ public class CtrlDominiInterficie {
                     ArrayList<ArrayList<String> > aux = new ArrayList<ArrayList<String> >();
                     aux = consultarLlibresEstanteria(e.get(i).getID());
                     for (int j = 0; j < aux.size(); ++j) {
-                        aux.get(j).add(Integer.toString(e.get(i).getPosX()));
-                        aux.get(j).add(Integer.toString(e.get(i).getPosY()));
+                        aux.get(j).add(Integer.toString(e.get(i).getPosX())+","+Integer.toString(e.get(i).getPosY()));
                         result.add(aux.get(j));
                     }
                 }
