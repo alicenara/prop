be5b6359339c703ca86a72a123da7100d91b2e99
Towerthousand <dani.torramilans@gmail.com>
2014-06-05 23:42:51 +0200

    Centrado

--- src/propLlibreria/Interficie/VistaPrincipal.java
+++ src/propLlibreria/Interficie/VistaPrincipal.java
@@ -49,6 +49,8 @@ public final class VistaPrincipal extends javax.swing.JFrame {
         vConsultes = new VistaConsultes();
         vOrd = new VistaOrdenacio();
         this.ferVisiblePrincipal();
+        java.awt.Dimension dim = java.awt.Toolkit.getDefaultToolkit().getScreenSize();
+        this.setLocation(dim.width/2-this.getSize().width/2, dim.height/2-this.getSize().height/2);
     }
     
     /**
